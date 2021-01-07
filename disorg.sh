e0cb40ac8358a89a42e61458559b0ae57b2624c2
lincoln <lincolnauster@gmail.com>
2021-01-07 15:03:49 -0700

    added DEBUG config option and cleaned file tree

--- disorg.sh
+++ disorg.sh
@@ -4,11 +4,15 @@ BASE_DIRECTORY="$HOME/.disorg"
 DATE_FORMAT="D-M-Y"
 TIME_FORMAT="H:M"
 FOUR_DIGIT_YEAR="TRUE"
+DEBUG="FALSE"
 source ~/.config/disorg/disorg
 
 export FOUR_DIGIT_YEAR
 export DATE_FORMAT
 export TIME_FORMAT
 
-echo -e "$(find $BASE_DIRECTORY -name '*.ev')" | valgrind --leak-check=full $(dirname $0)/disorg-main "$@"
-# echo -e "$(find $BASE_DIRECTORY -name '*.ev')" | $(dirname $0)/disorg-main "$@"
+if [[ "$DEBUG" == "TRUE" ]]; then
+	echo -e "$(find $BASE_DIRECTORY -name '*.ev')" | valgrind --leak-check=full $(dirname $0)/disorg-main "$@"
+else
+	echo -e "$(find $BASE_DIRECTORY -name '*.ev')" | $(dirname $0)/disorg-main "$@"
+fi
