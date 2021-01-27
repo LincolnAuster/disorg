ae95f3da5786beae94cc82eddc9bac0770adf85d
lincoln <lincolnauster@gmail.com>
2021-01-27 10:32:44 -0700

    use functions for tests

--- tests/tests.c
+++ tests/tests.c
@@ -6,6 +6,8 @@
 #include "../src/conf.h"
 #include "../src/util.h"
 
+static int passed, total;
+
 struct KeyValueTest {
 	char *s;
 	struct KeyValue kv;
@@ -32,16 +34,14 @@ static const struct BufferAppendTest buffer_append_tests[] = {
 	{ "thi ", '\0', 4, "thi " }
 };
 
-int
-main(void)
+/* access static vars to test respective functions */
+static void key_value_read_test(void);
+static void buffer_append_test(void);
+
+static void
+key_value_read_test(void)
 {
-	int passed, total, count;
-	passed = total = 0;
-
-	printf("TESTING: util.h\n");
-	printf("         key_value_read\n");
-
-	count = sizeof(key_value_read_tests) /
+	int count = sizeof(key_value_read_tests) /
 	                           sizeof(key_value_read_tests[0]);
 
 	for (int i = 0; i < count; i++) {
@@ -49,7 +49,7 @@ main(void)
 		struct KeyValue *result = key_value_read(test.s);
 
 		if (KEY_VALUE_CMP((*result), test.kv) == 0) {
-			printf("             PASSED `%s`\n", test.s);
+			printf("PASSED `%s`\n", test.s);
 			passed++;
 		} else {
 			printf("%s failed, gave %s:%s\n",
@@ -58,17 +58,20 @@ main(void)
 		total++;
 		free(result);
 	}
+}
 
-	printf("         buffer_append\n");
-
-	count = sizeof(buffer_append_tests) / sizeof(buffer_append_tests[0]);
+static void
+buffer_append_test(void)
+{
+	int count = sizeof(buffer_append_tests) /
+	           sizeof(buffer_append_tests[0]);
 	for (int i = 0; i < count; i++) {
 		struct BufferAppendTest test = buffer_append_tests[i];
 		char *s = strdup(test.a);
 		buffer_append(&s, test.b, &test.s);
 
 		if (strcmp(s, test.r) == 0) {
-			printf("            PASSED: `%s`\n", test.r);
+			printf("PASSED: `%s`\n", test.r);
 			passed++;
 		} else {
 			printf("%s failed, gave %s", test.r, s);
@@ -77,6 +80,19 @@ main(void)
 		total++;
 		free(s);
 	}
+}
+
+int
+main(void)
+{
+	passed = total = 0;
+
+	printf("TESTING: util.h\n");
+	printf("         key_value_read\n");
+	key_value_read_test();
+
+	printf("         buffer_append\n");
+	buffer_append_test();
 
 	printf("         buffer_append_str\n");
 
